3af7400ed01323301c8fd7fd62eed7aaa1f53e72
MaptimeCapeCod <maptimecapecod@gmail.com>
2015-06-11 09:23:51 -0400

    created sites.js

---
+++ sites.js
@@ -0,0 +1,357 @@
+var points = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "A",
+        "Name": "Old Jail Lane Complex",
+        "Text": "Clustered on either side of Old Jail Lane are (no surprise) a number of buildings and sites that relate to Barnstable’s jails. The “Jail House” (#2917) has half of the first jail keeper’s house incorporated into its front part. A later jail keeper’s house is across the lane (19 Old Jail Lane), and behind it are the sites of the first and second Barnstable jails (site B). Nextdoor- but-one to the “Jail House” is the former home of Joseph Bursley (#2939), who, in addition to being a jail keeper, helped save many of the records from the burning County House in 1827.",
+        "Number": 1
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.31252145767212,
+          41.7037420429833
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "B",
+        "Name": "Barnstable Jails",
+        "Text": "",
+        "Number": 2
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.31239807605743,
+          41.703617886429655
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "C",
+        "Name": "Shearjashub Bourne Law Office - #2984",
+        "Text": "This house, originally built in 1717, once had a lean-to on its east end in which the Barnstable-born lawyer, Shearjashub Bourne, had his office. Later in his career, Bourne was one of the delegates in the 1788 Massachusetts convention to ratify the constitution.",
+        "Number": 3
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.31045347452164,
+          41.704741294258326
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "D",
+        "Name": "County Court House - #3016",
+        "Text": "The second courthouse in Barnstable was built in 1774 and served in that capacity until the burning of the County House persuaded County Commissioners to erect a more fire-proof building for their records and operations. A “Band of Patriots” in 1774 blocked the opening of the court to protest an attempt by the Crown to control the judicial system, and in 1776 and 1812, the courthouse served as a marshaling point for Barnstable soldiers before marching to war. In 1842, the building was sold to the Third Baptist Society, and in a strangely satisfying process was not only turned into a church but also turned on its site so that its front faced away from Main Street. The building now houses Tales of Cape Cod, a non-profit historical",
+        "Number": 4
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.30822455883026,
+          41.70422464924542
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "E",
+        "Name": "Judge Joseph M. Day House - #3060",
+        "Text": "This property has an interesting political past, one of the residents, a “Widow Nabby,” having been tarred and feathered by “radical whigs” outraged by her “outspoken tory” stance. The current house was built in 1806 and was a tavern as well as a home. Joseph M. Day, who was a Judge of Probate and had a law office in the village with his son Thomas, bought the property in",
+        "Number": 5
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.30799120664597,
+          41.70395431008425
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "F",
+        "Name": "W.F. Jones House - #3087",
+        "Text": "The building that houses the Barnstable Historical Society was once the home of Attorney Smith K. Hopkins. Originally from Truro, Hopkins was a Barnstable County trial judge from 1867 to 1889 and Clerk of the Courts from 1876. Prior to his law career, Hopkins saw significant action in several Civil War battles as part of the U.S. Navy. At his funeral in 1904 were Thomas Day, Alfred Crocker and Frederick Swift, who all appear on this pathway.",
+        "Number": 6
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.30811995267868,
+          41.70329547877559
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "G",
+        "Name": "Judge Daniel Davis House - #3074",
+        "Text": "Daniel Davis has the distinction of having served as a Justice of the Peace for two countries: in 1770, he was commissioned under King George III, and in 1775, he was commissioned under the Government and People of Massachusetts Bay. His stern portrait hangs in Sturgis Library, as he was Elizabeth Sturgis’s grandfather. According to records, the judge died in his house, but there have been no reports of his ghost haunting the property.",
+        "Number": 7
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.30769348144531,
+          41.703579838405545
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "H",
+        "Name": "County House - #3096",
+        "Text": "The old stone stairway in the wall once led up to the County office, which held the deeds, probate and other court records (the first courthouse, built around 1686, was located across the street, east of the Crocker Tavern, site I, #3095). On 22 October, 1827, a fire broke out. Several gentlemen enjoying a drink in the “hotel” across the road, including Captain John Bursley (see site A), rushed over and were able to save most of the probate records and one book of deeds, quickly snatched off a desk in the burning Registry of Deeds office through a hole broken into the second-floor window. In all, 93 folios of deeds, three folios of probate records and the court records burned up with the building.",
+        "Number": 8
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.3063416481018,
+          41.70285291656821
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "I",
+        "Name": "Crocker Tavern - #3095",
+        "Text": "",
+        "Number": 9
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.30706584453583,
+          41.70242436926547
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "J",
+        "Name": "Capt. Joshua Loring House - #3137",
+        "Text": "The Attorney Thomas C. Day was the son of Judge Joseph M. Day. By 1891, he had purchased the old Loring homestead, and he was living in this house during the time of one of his most sensational cases: the murder trial of Yarmouth resident Edwin Ray Snow in 1900 (see reverse). Thomas and his father Joseph also shared a law office at 3206 Main St. (site L, #3206), and there is a lawyer’s office there today, as well.",
+        "Number": 10
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.30587762594223,
+          41.702129991659696
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "K",
+        "Name": "Barnstable County Superior Courthouse - #3195",
+        "Text": "This courthouse, constructed in 1831-32 out of Quincy granite and designed by architect Alexander Parris, was the third one built in the village. Among the more notable figures who appeared in its courtroom was lawyer Daniel Webster. Out front are cannons brought to Barnstable from Boston to deter the British during the war of 1812, and statues of patriots Mercy Otis Warren and her brother James Otis. The impressive columns are deceptive: although they look like blocks of stone, they’re actually painted wood.",
+        "Number": 11
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.30541896820068,
+          41.70157527645452
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "L",
+        "Name": "Law Office - #3206",
+        "Text": "",
+        "Number": 12
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.30479937791824,
+          41.702188066260064
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "M",
+        "Name": "Alfred Crocker House - #3310",
+        "Text": "Alfred Crocker was Clerk of the Superior Court in Barnstable during the late 19th and early 20th centuries. He bought the house with its accompanying store (since removed) in the 1880s. There was once a large scale for weighing carts at the side of the house on Mill Way (his great-grandson David remembers standing on it in the 1940s). Crocker’s grandfather, Loring, obtained the cannons (see site K) used to deter the British from attacking his saltworks in 1812.",
+        "Number": 13
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.30037373304367,
+          41.70057397335584
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "N",
+        "Name": "Cobb's Hill Cemetery - Route 6A & Mill Way",
+        "Text": "This cemetery, divided into East (the older section) and West, contains the gravestones of several residents of the village who were associated with the judicial system. Of special interests are the graves of John Bacon, Esq. (died 1731) and John Gorham, Esq., whose 1715 stone is the oldest in the cemetery. Gorham was “Coll [colonel] of the Regiment and one of His Majesties Justices of the Peace in the County of Barnstable…Here Lyes A Valiant Soldeir And A Saint. A Judge, A Justice, Whome no Vice could taint...”",
+        "Number": 14
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.2997836470604,
+          41.70054193139926
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "O",
+        "Name": "Old Jail - #3365",
+        "Text": "In 1973, Barnstable’s first jail was moved from its original site on Old Jail Lane (site B). Built in 1690 and used for 130 years, the wooden jail is the oldest such structure in the United States and housed probably no more than six prisoners at a time. Inside, there are drawings of ships and other graffiti scratched into the wood by former prisoners.",
+        "Number": 15
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.29956102371216,
+          41.700245542543804
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "P",
+        "Name": "Charles Sturgis House - #3576",
+        "Text": "Prior to Charles Sturgis purchasing the home in 1839, at one time the land on which is sits belonged to John Bacon, Esq. (1661-1731), a judge of the Court of Common Pleas and a lawyer with a large practice. In Bacon’s will, he leaves his law book to his son Solomon, and in a morally-ambiguous fashion (at least to modern sensibilities), he directs that his slave Dinah should be sold and the proceeds used to buy bibles for his grandchildren. According to chronicler Amos Otis, John Bacon “...wrote the worst hand, for a man of business, that I have ever met with; his lines were crooked in every direction; his letters cramped and awkwardly formed…” His gravestone can be found in Cobb’s Hill East Cemetery.",
+        "Number": 16
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.29205620288849,
+          41.70110666854529
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "Q",
+        "Name": "Charles Swift House - #35",
+        "Text": "This house was built by one of Yarmouth Port’s more interesting 19th century inhabitants. Charles F. Swift was probably best known as the publisher of the Yarmouth Register, but he also served as a representative in the State Legislature and as interim Register of Probate for Barnstable County. Furthermore, during his time in the state House of Representatives in the 1880s, he chaired the Committee on Prisons. Swift was a firm Republican, and when he became editor of the Register, he continued the ongoing political feud with the competing local paper, the Barnstable Patriot, which supported the Democrats. This literary battle could apparently be quite nasty; Swift himself describes it as “exceedingly bitter and personal.” Nevertheless, the writer of Swift’s 1903 obituary in the Patriot states that he “feels the loss of a personal friend—one who had been to us in earlier years a pleasant employer, later a worthy opponent, and still later a true friend…”",
+        "Number": 17
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.25763273239136,
+          41.70143108976923
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "R",
+        "Name": "Frederick Hallett - #112",
+        "Text": "Frederick Hallett was only 25 years old when he died in 1861, but prior to that, he had studied with Judge Joseph Day, been admitted to the bar and begun his law practice. According to the History of Barnstable County, Hallett was “universally beloved and when he died, Yarmouth, as a town, put on its sincere mourning,” and the Barnstable Patriot describes him as “the late lamented Frederick Hallett.” He is listed as living in this house in the 1858 map of Cape Cod, Martha’s Vineyard and Nantucket. The house is currently a bed and breakfast called the King’s Inne.",
+        "Number": 18
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.25318026542664,
+          41.70386219425859
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "S",
+        "Name": "Frederick C. Swift House - #165",
+        "Text": "Son of editor Charles F. Swift, Frederick Swift is listed as inhabiting this house in 1895, although the structure itself dates from around the late 1850s. Swift was a lawyer and a justice of the peace, who at one time worked in the office of Barnstable’s Judge Joseph Day. In 1918, he was a witness in the appeal trial of Edwin Ray Snow (see site T) and was strongly of the opinion that Snow should stay behind bars. His own office at one point was in the Register building, which must have proved handy, as during his father’s tenure in the House of Representatives in the early 1880s, Swift had editorial charge of the Yarmouth Register newspaper.",
+        "Number": 19
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.25156021118164,
+          41.70358184093369
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "T",
+        "Name": "Capt. James Bacon Crocker House - #168",
+        "Text": "The Blushing Oyster Bed & Breakfast now occupies the former home of Barnstable-born Captain James Bacon Crocker, who had the house built in 1843. After leaving the sea, Crocker got involved in all sorts of other work, including acting as clerk of the Barnstable County courts and serving as a trial Justice. His talents don’t seem to have been limited to his working life: his 1883 obituary in the Barnstable Patriot notes that he “was a man of...remarkably pleasing social qualities.”",
+        "Number": 20
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.25157630443573,
+          41.70396632518221
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "U",
+        "Name": "Edwin Ray Snow House - #176",
+        "Text": "On 13 September, 1899, 17-year-old Edwin Ray Snow shot and killed his distant family member and acquaintance, 21-year-old Jimmy Whittemore. Whittemore, a baker’s delivery boy, had encountered Snow by chance on his route and taken him along. Snow, who had been in and out of trouble for a few years and who desperately needed money, was carrying his father’s pistol. Later in the day, he pulled the gun on Whittemore, demanding the money collected from the deliveries. When Whittemore refused, he shot him. Although sentenced to die in the newly-invented electric chair, Snow’s sentence was commuted. Thomas C. Day of Barnstable was Snow’s attorney, but his connection to the family predates the trial: in 1895, Day bought the Snow family’s house with the provision that they could purchase it back from him within a year for the same price plus interest. They must have paid him promptly, since the house was in Edwin’s mother Ida’s possession at the time of her death in 1923.",
+        "Number": 21
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.25114178657532,
+          41.70409849111166
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Site": "V",
+        "Name": "Tarmouth Port Library - #297",
+        "Text": "Yarmouth Port Library has several ties to the law. The very first building was formerly the law office of the late Hon. John Reed, given to the officers of the newly-formed Yarmouth Library by the Reverend Nathaniel Cogswell (a further tie: Cogswell’s son, John Bear Doane Cogswell, was a lawyer and later a representative in the Massachusetts Legislature and the president of the State Senate). Among these officers was Register editor and future legislator, Charles F. Swift. The building was moved to the current Library site in August, 1866 and opened that December. The law office proved to be insufficient to the literary needs of the village, however; the main Library building was replaced a mere two years later.",
+        "Number": 22
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.24112641811371,
+          41.706193084570906
+        ]
+      }
+    }
+  ]
+}
